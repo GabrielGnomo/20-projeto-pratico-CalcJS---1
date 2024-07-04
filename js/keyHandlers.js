1858bb4a046c36fe4169d71a5ce787eba52c270e
Gabriel Black <gabrieloliveiras2306@hotmail.com>
2024-07-04 12:59:57 -0300

    Initial commit

---
+++ js/keyHandlers.js
@@ -0,0 +1,50 @@
+import calculate from "./calculate.js";
+
+const input = document.querySelector("#input");
+
+function handleButtonPress(ev) {
+  const value = ev.currentTarget.dataset.value;
+  input.value += value;
+}
+
+function handleClearButton() {
+  input.value = "";
+  input.focus();
+}
+
+function handleTyping(ev) {
+  ev.preventDefault();
+  const allowedKeys = [
+    "(",
+    ")",
+    "/",
+    "*",
+    "-",
+    "+",
+    "9",
+    "8",
+    "7",
+    "6",
+    "5",
+    "4",
+    "3",
+    "2",
+    "1",
+    "0",
+    ".",
+    "%",
+    " ",
+  ];
+  if (allowedKeys.includes(ev.key)) {
+    input.value += ev.key;
+    return;
+  }
+  if (ev.key === "Backspace") {
+    input.value = input.value.slice(0, -1);
+  }
+  if (ev.key === "Enter") {
+    calculate();
+  }
+}
+
+export { handleButtonPress, handleClearButton, handleTyping };
